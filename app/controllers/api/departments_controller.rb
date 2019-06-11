f9ce010d7d5b8066fc45dbbe085aeb1882868b31
Melissa G <jstmac@gmail.com>
2019-06-11 17:26:47 -0600

    Departments are rendered

--- app/controllers/api/departments_controller.rb
+++ app/controllers/api/departments_controller.rb
@@ -10,7 +10,7 @@ class Api::DepartmentsController < ApplicationController
   end
 
   def create
-    Department = Department.new(department_params)
+    department = Department.new(department_params)
 
     if department.save
       render json: department
--- client/src/components/Departments.js
+++ client/src/components/Departments.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { Card, Header } from "semantic-ui-react";
+import axios from "axios";
 
-const Departments = () => <h1>Departments</h1>;
+// const Departments = () => <h1>Departments</h1>;
+
+class Departments extends React.Component {
+  state = { departments: [] };
+
+  componentDidMount() {
+    axios.get("/api/departments").then(res => {
+      this.setState({ departments: res.data });
+    });
+  }
+
+  renderDepartments = () => {
+    const { departments } = this.state;
+
+    if (departments.length <= 0) return <h2>No Departments</h2>;
+    return departments.map(department => (
+      <Card>
+        <Card.Content>
+          <Card.Header>{department.name}</Card.Header>
+          <Card.Meta>{department.description}</Card.Meta>
+          <Card.Description>{department.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    ));
+  };
+
+  render() {
+    return (
+      <div>
+        <Header as='h1'>Departments</Header>
+        <br />
+        <Card.Group>{this.renderDepartments()}</Card.Group>
+      </div>
+    );
+  }
+}
 
 export default Departments;
