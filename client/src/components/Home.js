1a09b94eec012beb40c272bdc7474bb49c952a76
Melissa <jstmac@gmail.com>
2019-06-13 00:00:43 -0600

    Added Pic to Home Page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,30 +1,33 @@
 import React from "react";
-import { Header, Button, Segment, Card, Icon } from "semantic-ui-react";
-import styled from "styled-components";
+import axios from "axios";
 import HeaderText from "./HeaderText";
+import styled from "styled-components";
+import { Header, Segment, Image } from "semantic-ui-react";
+import Pic from "./departmentstore.jpg";
 
 const Home = () => (
-  <HomeContainer>
-    <HeaderText fSize='large'>Departments R Us!!</HeaderText>
-    <Segment as={Transparent}>
-      <Header as={HeaderText}>
-        No Need to Look Elsewhere-It's All Here 4 U
-      </Header>
-    </Segment>
-    <Segment as={Transparent}>
-      <Header as={HeaderText}>Check us out!</Header>
-    </Segment>
-  </HomeContainer>
+  <>
+    <HomeContainer>
+      <HeaderText fSize='large'>Departments R Us!!</HeaderText>
+      <Segment as={Transparent}>
+        <Header as={HeaderText}>
+          No Need to Look Elsewhere-It's All Here 4 U
+        </Header>
+      </Segment>
+      <Segment as={Transparent}>
+        <Header as={HeaderText}>Check us out!</Header>
+      </Segment>
+    </HomeContainer>
+    <Image src={Pic} alt='Department Store' centered />
+  </>
 );
 
 const HomeContainer = styled.div`
-  background: linear-gradient(to bottom right, aliceblue, black);
+  background: linear-gradient(to bottom right, aliceblue, blue);
 `;
 
 const Transparent = styled.div`
   background: transparent !important;
 `;
-
-// const Home = () => <h1>Departments R Us!!</h1>;
 
 export default Home;
